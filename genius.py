83ad061a8479863e5e96fcc338a33909acaaa769
xmasalov <xmasalov@gmail.com>
2017-06-21 02:30:54 +0400

    Quick Fixes
    Some quick fixes:
    
    Description is more explicit now on how to get a token without creating a page;
    Fixed indenation;
    Removing script tags in the middle of the lyrics is needless anymore because there is none any;
    [h.extract() for h in html('script')]
    Finding a class 'lyrics' was fixed;
    lyrics = html.find("lyrics").get_text()
    lyrics = html.find(class_='lyrics').get_text()
    Other small fixes.

--- genius.py
+++ genius.py
@@ -1,48 +1,41 @@
-'''
-GENIUS LYRICS ANALYSIS
+'''GENIUS LYRICS SCRAPER 
+* Python 3
+* For informational purposes only
+** Based on this blog post: https://tinyurl.com/geniusscraper
 
 Setup:
-1. get an account at genius.com
-2. suffer
-3. created html page to get token
-4. had to have 'bearer
-5. had to change data=data to params=data
-6.installing bs4 - had to do as admin
+1. Get an account at http://genius.com/. 
+2. Get your token at http://genius.com/api-clients or... You can use the client access token associated 
+with your API instead of tokens for authenticated users, get one at https://docs.genius.com/. These 
+tokens are only valid for read-only endpoints that are not restricted by a required scope.
 '''
 
 import requests
 from bs4 import BeautifulSoup
 
 base_url = 'http://api.genius.com'
-headers = {'Authorization': 'Bearer TOKEN'}
+headers = {'Authorization': 'Bearer YOURTOKENHERE'}
 
-artist_name = "Tom Misch"
-song_title = "in the midst of it all"
+song_title = 'Sad but True'
+artist_name = 'Metallica'
 
 def lyrics_from_song_api_path(song_api_path):
-  song_url = base_url + song_api_path
-  response = requests.get(song_url, headers=headers)
-  json = response.json()
-  path = json["response"]["song"]["path"]
+    song_url = base_url + song_api_path
+    response = requests.get(song_url, headers=headers)
+    json = response.json()
+    path = json['response']['song']['path']
 
-  #gotta go regular html scraping... come on Genius
-  page_url = "http://genius.com" + path
-  page = requests.get(page_url)
-  html = BeautifulSoup(page.text, "html.parser")
+    # Regular HTML scraping
+    page_url = 'http://genius.com' + path
+    page = requests.get(page_url)
+    html = BeautifulSoup(page.text, 'html.parser')
+    lyrics = html.find(class_='lyrics').get_text()
+    return lyrics
 
-  #remove script tags that they put in the middle of the lyrics
-  [h.extract() for h in html('script')]
-
-  #at least Genius is nice and has a tag called 'lyrics'!
-  lyrics = html.find("lyrics").get_text()
-  return lyrics
-
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     search_url = base_url + '/search'
     data = {'q': song_title}
     response = requests.get(search_url, params=data, headers=headers)
-
     song_info = None
     json = response.json()
 
@@ -51,7 +44,7 @@ if __name__ == "__main__":
             song_info = hit
     if song_info:
         song_api_path = song_info['result']['api_path']
-        print (lyrics_from_song_api_path(song_api_path))
-
-
-
+        print(song_info['result']['full_title'])
+        print(lyrics_from_song_api_path(song_api_path))
+    else:
+        print(artist_name + ' - ' + song_title + ': Not found')
